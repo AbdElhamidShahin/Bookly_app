e2b4f1932fb3b892a8c73790d5fed164f615be0c
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-09-09 23:29:21 +0300

    add animation to splash Scrren and add constans

---
+++ lib/presentation/screens/SplashPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'SplashVeiwBody.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SplashVeiwBody());
+  }
+}
---
+++ lib/presentation/screens/SplashVeiwBody.dart
@@ -0,0 +1,70 @@
+import 'package:booky_app/presentation/screens/HomeView.dart';
+import 'package:booky_app/presentation/screens/SplashPage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../core/constants/Strings.dart';
+
+class SplashVeiwBody extends StatefulWidget {
+  const SplashVeiwBody({super.key});
+
+  @override
+  State<SplashVeiwBody> createState() => _SplashVeiwBodyState();
+}
+
+class _SplashVeiwBodyState extends State<SplashVeiwBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationTextSplash();
+    navigatorPushHome();
+  }
+
+  void navigatorPushHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: kTranstionDuration,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/Logo.png"),
+        SizedBox(height: 8),
+        SlideTransition(
+          position: slidingAnimation,
+          child: Text(
+            "Read Free Books",
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void animationTextSplash() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 3),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+}
