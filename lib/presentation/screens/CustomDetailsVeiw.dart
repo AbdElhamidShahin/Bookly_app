3e5a6fc287d6a1204af568af0683b8b77ca61d88
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-09-15 23:10:03 +0300

    add SimilerBooksListVeiw and SliverScrollVeiw

--- lib/presentation/screens/CustomDetailsVeiw.dart
+++ lib/presentation/screens/CustomDetailsVeiw.dart
@@ -1,8 +1,10 @@
 import 'package:booky_app/core/utils/style.dart';
 import 'package:booky_app/presentation/screens/CustomDetailsVeiw.dart';
 import 'package:booky_app/presentation/widgets/BooksRating.dart';
+import 'package:booky_app/presentation/widgets/CustomButton.dart';
 import 'package:booky_app/presentation/widgets/CustomDetailsScreenAppbar.dart';
 import 'package:booky_app/presentation/widgets/CustomListVeiwItem.dart';
+import 'package:booky_app/presentation/widgets/SimilerBooksListVeiw.dart';
 import 'package:flutter/material.dart';
 
 class CustomDetailsScreen extends StatelessWidget {
@@ -12,60 +14,87 @@ class CustomDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var weight = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: Column(
-        children: [
-          CustomDetailsScreenAppbar(),
-
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: weight * 0.25),
-            child: CustomListVeiwItem(),
-          ),
-          SizedBox(height: 32),
-          Text("The Jungle Book", style: Styles.textStyle30),
-          SizedBox(height: 6),
-
-          Opacity(
-            opacity: .7,
-            child: Text(
-              "Rudyard Kiping",
-              style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-
-          SizedBox(height: 24),
-          BooksRating(mainAxisAlignment: MainAxisAlignment.center),
-          SizedBox(height: 36),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 38),
-            child: Row(
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Column(
               children: [
-                Expanded(
-                  child: CustomButton(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      bottomLeft: Radius.circular(16),
+                CustomDetailsScreenAppbar(),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: weight * 0.25),
+                  child: CustomListVeiwItem(),
+                ),
+                SizedBox(height: 32),
+                Text("The Jungle Book", style: Styles.textStyle30),
+                SizedBox(height: 6),
+
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    "Rudyard Kiping",
+                    style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
                     ),
-                    backgroundColor: Colors.white,
-                    text: "19.99\$",
-                    fontSize: 16,
-                    textColor: Colors.black,
                   ),
                 ),
-                Expanded(
-                  child: CustomButton(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(16),
-                      bottomRight: Radius.circular(16),
-                    ),
-                    backgroundColor: Color(0xffEF8262),
-                    text: "Free PreView",
-                    fontSize: 16,
-                    textColor: Colors.white,
+
+                SizedBox(height: 24),
+                BooksRating(mainAxisAlignment: MainAxisAlignment.center),
+                SizedBox(height: 36),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 38),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CustomButton(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            bottomLeft: Radius.circular(16),
+                          ),
+                          backgroundColor: Colors.white,
+                          text: "19.99\$",
+                          fontSize: 16,
+                          textColor: Colors.black,
+                        ),
+                      ),
+                      Expanded(
+                        child: CustomButton(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(16),
+                            bottomRight: Radius.circular(16),
+                          ),
+                          backgroundColor: Color(0xffEF8262),
+                          text: "Free PreView",
+                          fontSize: 16,
+                          textColor: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+
+                Expanded(child: SizedBox(height: 50)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "You Can Also Like",
+                      style: Styles.textStyle14.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: SimilerBooksListVeiw(),
+                ),
+                SizedBox(height: 40),
               ],
             ),
           ),
---
+++ lib/presentation/widgets/SimilerBooksListVeiw.dart
@@ -0,0 +1,24 @@
+
+import 'package:booky_app/presentation/widgets/CustomListVeiwItem.dart';
+import 'package:flutter/material.dart';
+
+class SimilerBooksListVeiw extends StatelessWidget {
+  const SimilerBooksListVeiw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .15,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: CustomListVeiwItem(),
+          );
+        },
+      ),
+    );
+    ;
+  }
+}
