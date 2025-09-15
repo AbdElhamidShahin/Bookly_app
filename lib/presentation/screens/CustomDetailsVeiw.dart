881afca4d5bfdb508ca4f3e14c4d01bc4ad91e32
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-09-15 21:28:57 +0300

    add CustomDetailsScreen veiw body

--- lib/presentation/screens/CustomDetailsVeiw.dart
+++ lib/presentation/screens/CustomDetailsVeiw.dart
@@ -1,5 +1,6 @@
-import 'package:booky_app/presentation/screens/CustomDetailsVeiw.dart';
+import 'package:booky_app/core/utils/style.dart';
 import 'package:booky_app/presentation/widgets/CustomDetailsScreenAppbar.dart';
+import 'package:booky_app/presentation/widgets/CustomListVeiwItem.dart';
 import 'package:flutter/material.dart';
 
 class CustomDetailsScreen extends StatelessWidget {
@@ -7,6 +8,32 @@ class CustomDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(children: [CustomDetailsScreenAppbar()]));
+    var weight = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Column(
+        children: [
+          CustomDetailsScreenAppbar(),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: weight * 0.20),
+            child: CustomListVeiwItem(),
+          ),
+          SizedBox(height: 42),
+          Text("The Jungle Book", style: Styles.textStyle30),
+          SizedBox(height: 6),
+
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rudyard Kiping",
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
